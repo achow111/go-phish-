3387405dac0b11fe36d4877a81135c82036fe58a
sahejepoch <99779795+Sodhi-S@users.noreply.github.com>
2024-11-09 22:50:45 -0500

    added collaborator in leaderboard for comparison

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -1,20 +1,20 @@
 import BoardRow from '../components/boardrows';
 
 export const Leaderboard = () => {
-    return (
-      <div>
-        <h1>Welcome to the Leaderboard Page!</h1>
-        <div className="leaderboard-row">
-          <span className="position">Place</span>
-          <span className="username">Username</span>
-          <span className="score">Score</span>
-        </div>
-        <BoardRow position={1} username="Abeer Das" score={420} />
-        <BoardRow position={2} username="Aaron Chow" score={69} />
-        <BoardRow position={3} username="Sahej Singh Sodhi" score={12} />
+  return (
+    <div>
+      <h1>Welcome to the Leaderboard Page!</h1>
+      <div className="leaderboard-row">
+        <span className="position">Place</span>
+        <span className="username">Username</span>
+        <span className="score">Score</span>
       </div>
-    );
-  };
-  
-  export default Leaderboard;
-  
+      <BoardRow position={1} username="Abeer Das" score={420} />
+      <BoardRow position={2} username="Aaron Chow" score={69} />
+      <BoardRow position={3} username="Sahej Sodhi" score={57} />
+      <BoardRow position={4} username="Megh Patel" score={46} />
+    </div>
+  );
+};
+
+export default Leaderboard;
