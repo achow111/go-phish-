a1c49b268272502e0daf1f5b3d06a9a3194c9578
Megh <hirparamegh@gmail.com>
2024-11-09 23:04:36 -0500

    updates

--- phish_detector/api/models.py
+++ phish_detector/api/models.py
@@ -3,6 +3,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+point_system = {
+    'common': 4,
+    'rare': 7,
+    'mythic': 10,
+    'legendary': 18
+}
+
+
+
 class Employee(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     organization = models.CharField(max_length=100,blank=True, null=True)
@@ -11,4 +20,12 @@ class Employee(models.Model):
     def __str__(self):
         return self.user.username
 
+    @property
+    def score(self):
+        result = 0
+        
+        for k, v in self.fish.items():
+            result += v * point_system[k]
+            
+        return result
 
--- phish_detector/phish_detector/urls.py
+++ phish_detector/phish_detector/urls.py
@@ -1,16 +1,8 @@
 from django.contrib import admin
-<<<<<<< Updated upstream
-from django.urls import path
-from . import views  # Import your views (if necessary)
-
-urlpatterns = [
-    path('admin/', admin.site.urls),  # Admin route
-    # Add other URL patterns here
-=======
 from django.urls import path, include
+from . import views  # Import your views (if necessary)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('api.urls')),
->>>>>>> Stashed changes
 ]
